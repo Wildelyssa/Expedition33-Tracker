7963fdfa573c83d58b253c58b36c3c3576b786ee
Wildelyssa <lysa.jones@gmail.com>
2025-08-11 18:17:41 +0100

    add game logo to page

--- app/sections/navigation/Navigation.tsx
+++ app/sections/navigation/Navigation.tsx
@@ -1,13 +1,23 @@
+import Image from "next/image";
 import { sections } from "../data/progressData";
 import NavItem from "./NavItem";
 
 const Navigation = () => {
   return (
     // hashlink to sections with interval observer
-    <div className="w-full flex flex-row gap-2 p-2 border border-yellow-300/40 rounded-md">
-      {sections.map((item, i) => (
-        <NavItem key={`${item.id}${i}`} id={item.id} text={item.text} />
-      ))}
+    <div className="flex flex-col items-center gap-4">
+      <Image
+        src="/images/branding/game-logo.png"
+        width={500}
+        height={100}
+        alt="Clair Obscur:Expedition 33"
+      />
+
+      <div className="w-full flex flex-row gap-2 p-2 border border-yellow-300/40 rounded-md">
+        {sections.map((item, i) => (
+          <NavItem key={`${item.id}${i}`} id={item.id} text={item.text} />
+        ))}
+      </div>
     </div>
   );
 };
